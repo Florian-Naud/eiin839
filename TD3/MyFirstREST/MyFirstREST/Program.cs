8bf10b8294d9d97e6d4835fcc45f239fd1acb347
Florian-Naud <florian.naud@etu.unice.fr>
2021-02-24 16:31:58 +0100

    Add program for Q1

---
+++ TD3/MyFirstREST/MyFirstREST/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace MyFirstREST
+{
+    class Program
+    {
+        // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+        static readonly HttpClient client = new HttpClient();
+
+        static void Main()
+        {
+           
+            try
+            {
+                String res = getAllContract().Result;
+                //res.ToString()
+                Console.WriteLine(JsonConvert.DeserializeObject(res));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nRIP petit program!");
+            }
+            Console.ReadLine();
+            
+        }
+
+        public static async Task<String> getAllContract()
+        {
+            return await httpGetRequest("https://api.jcdecaux.com/vls/v1/stations?apiKey=aadb7f161ebe9eb9358509283772daab3be2898b");
+        }
+
+        public static async Task<String> httpGetRequest(String request)
+        {
+            HttpResponseMessage response = await client.GetAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+    }
+}
