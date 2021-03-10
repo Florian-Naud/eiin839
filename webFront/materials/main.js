83a2ece34c64b4edce0fc0879f52a761bd065d6d
Florian-Naud <florian.naud@etu.unice.fr>
2021-03-10 16:51:40 +0100

    webFront: exo 4

--- webFront/materials/main.js
+++ webFront/materials/main.js
@@ -1,3 +1,5 @@
+var stations;
+
 function requestGet(url, callback) {
     var request = new XMLHttpRequest();
     request.open("GET", url, true);
@@ -33,6 +35,40 @@ function retrieveContractStations() {
 }
 
 function stationsRetrieved() {
-    var response = JSON.parse(this.responseText);
-    console.log(response);
+    stations = JSON.parse(this.responseText);
+    console.log(stations);
+}
+
+function getDistanceFrom2GpsCoordinates(lat1, lon1, lat2, lon2) {
+    // Radius of the earth in km
+    var earthRadius = 6371;
+    var dLat = deg2rad(lat2 - lat1);
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = earthRadius * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+}
+
+function getClosestStation() {
+    var lat = document.getElementById("latitude").value;
+    var lon = document.getElementById("longitude").value;
+    var distance = null;
+    var name;
+    stations.forEach(station => {
+        var newDistance = getDistanceFrom2GpsCoordinates(lat, lon, station.position.lat, station.position.lng);
+        if (!distance || distance > newDistance) {
+            distance = newDistance;
+            name = station.name;
+        }
+    });
+
+    console.log(name);
 }
