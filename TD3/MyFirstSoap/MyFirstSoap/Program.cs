7d745dae085ad38c72702a3d3b9f4ef20e25953d
Florian-Naud <florian.naud@etu.unice.fr>
2021-03-10 16:35:18 +0100

    webFront: exo 1,2 and 3

---
+++ TD3/MyFirstSoap/MyFirstSoap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstSoap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorService.CalculatorSoapClient calc = new CalculatorService.CalculatorSoapClient();
+            int result = calc.Add(2, 3);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
--- webFront/materials/main.js
+++ webFront/materials/main.js
@@ -1,1 +1,38 @@
-document.getElementById("workingTest").innerHTML="It works";
+function requestGet(url, callback) {
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.setRequestHeader("Accept", "application/json");
+    request.onload = callback;
+    request.send();
+}
+
+function retrieveAllContracts() {
+    var key = document.getElementById("key").value;
+    var url = "https://api.jcdecaux.com/vls/v3/contracts?apiKey=" + key;
+
+    requestGet(url, contractsRetrieved);
+}
+
+function contractsRetrieved() {
+    var response = JSON.parse(this.responseText);
+    var list = document.getElementById("list-of-contracts");
+    response.forEach(contract => {
+        var option = document.createElement("option");
+        option.value = contract.name;
+        list.appendChild(option);
+    });
+
+}
+
+function retrieveContractStations() {
+    var key = document.getElementById("key").value;
+    var contract = document.getElementById("contracts").value;
+    var url = "https://api.jcdecaux.com/vls/v1/stations?contract=" + contract + "&apiKey=" + key;
+
+    requestGet(url, stationsRetrieved);
+}
+
+function stationsRetrieved() {
+    var response = JSON.parse(this.responseText);
+    console.log(response);
+}
